e548541c2e19eb12bc15dc50aebadd153b69af00
Marco Mazzoni <98465256+MarcoMazzoni93@users.noreply.github.com>
2024-09-23 14:57:42 +0200

    Update run_kallisto.sh

--- Scripts/run_kallisto.sh
+++ Scripts/run_kallisto.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+declare -a samples=( IV1 IV2 IV3 V2 V3 V4 VI1 VI2 VI3 VII1 VII2 VII3 )
+
+kallisto index -i ./Amel.idx ./Apis_mellifera.Amel_HAv3.1.cds.all.fa
+
+for i in "${array[@]}"; do
+  kallisto quant -i Amel.idx -o ${i} ./raw_data/${i}_1.fq.gz ./raw_data/${i}_2.fq.gz --bias
+done
